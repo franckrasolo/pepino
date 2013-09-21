46596bd70dd14c55f5ac4438732d5df1b18a8563
Franck Rasolo <franck.rasolo@gmail.com>
2013-09-21 18:59:35 +0100

    Implemented more types and some basic rendering

---
+++ core/src/Draft.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE QuasiQuotes #-}
+module Main where
+
+import Data.String.Here
+import Pepino
+
+main :: IO ()
+main = print feature
+
+feature :: Feature
+feature = Feature "<feature title>" [hereLit|
+    <feature description>
+|] (Background "" "") [ scenario ]
+
+scenario :: Scenario
+scenario = Scenario "<scenario title>" [hereLit|
+    A scenario
+    can be described
+    over multiple lines.
+|] $
+    Given "a first step" :
+    And   "a second step" :
+    But   "under different conditions" :
+    When  "an action is performed" :
+    Then  "a first condition holds true" :
+    But   "the second one doesn't" :
+    And   "all is good" :
+    []
--- core/src/Pepino.hs
+++ core/src/Pepino.hs
@@ -1,15 +1,39 @@
 module Pepino where
 
-newtype Title = String
-newtype Description = String
-newtype Sentence = String
+import Data.List (intercalate)
 
-data Feature = Feature Title Description Background [Scenario] deriving Show
-data Background = Background Title Description deriving Show
+type Title = String
+type Description = String
+type Sentence = String
 
-data Scenario = Scenario Title Description
-              | ScenarioOutline Title Description [Examples]
-            deriving Show
+data Feature = Feature Title Description Background [Scenario]
+data Background = Background Title Description
 
-data Step = Given Sentence | When Sentence | Then Sentence | And Sentence | But Sentence deriving Show
-data Examples = Examples Title Description deriving Show
+data Scenario = Scenario Title Description [Step]
+              | Outline  Title Description [Step] [Examples]
+
+data Step = Given Sentence | When Sentence | Then Sentence | And Sentence | But Sentence
+data Examples = Examples Title Description
+
+instance Show Feature where
+    show (Feature title description _ scenarios) = "Feature: " ++ title ++ format description ++ intercalate "\n" (map show scenarios)
+
+instance Show Background where
+    show (Background title description) = "Background: " ++ title ++ format description
+
+instance Show Scenario where
+    show (Scenario title description steps) = "Scenario: " ++ title ++ format description ++ intercalate "\n    " ("    " : (map show steps))
+    show (Outline  title description steps examples) = "Scenario Outline: " ++ title ++ format description ++ show steps ++ "\n" ++ show examples
+
+instance Show Examples where
+    show (Examples title description) = "Examples: " ++ title ++ format description
+
+instance Show Step where
+    show (Given sentence) = "Given " ++ sentence
+    show (When  sentence) = "When  " ++ sentence
+    show (Then  sentence) = "Then  " ++ sentence
+    show (And   sentence) = "And   " ++ sentence
+    show (But   sentence) = "But   " ++ sentence
+
+format :: Description -> String
+format description = "\n" ++ description ++ "\n"
