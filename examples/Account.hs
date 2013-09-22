83f5c48b2494212e19ef22c8573a5a33babbbc36
Franck Rasolo <franck.rasolo@gmail.com>
2013-09-22 20:17:31 +0100

    Removed usage of lenses which increased build time by 3 minutes!

--- examples/Account.hs
+++ examples/Account.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE TemplateHaskell #-}
 module Account where
 
-import Control.Lens
-
 type Amount = Double
-data Account = Account { _balance :: Amount } deriving (Eq, Show)
-makeLenses ''Account
+data Account = Account { balance :: Amount } deriving (Eq, Show)
 
 pay :: Amount -> Account -> Account
-pay amount account = (balance +~ amount) account
+pay amount account = account { balance = (balance account) + amount }
 
 withdraw :: Amount -> Account -> Account
-withdraw amount account = (balance -~ amount) account
+withdraw amount account = account { balance = (balance account) - amount }
 
 transfer :: Amount -> Account -> Account -> (Account, Account)
 transfer amount account1 account2 = (withdraw amount account1, pay amount account2)
